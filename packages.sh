85bc8838aa298d446f687abce43074755a114267
iPower <jan.krzan@icloud.com>
2016-08-28 21:15:35 +0200

    Added iPowerLockGlyph

--- packages.sh
+++ packages.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/iPower debs
 dpkg-deb -bZgzip projects/iPowerRepoIcons debs
+dpkg-deb -bZgzip projects/iPowerLockGlyph debs
+
--- pushiPhone.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-./dpkg-scanpackages debs /dev/null > Packages
-bzip2 -fks Packages
