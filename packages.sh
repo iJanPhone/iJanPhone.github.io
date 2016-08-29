9c5e63256e197197eb3d3f6c56de6624a52aef28
iPower <jan.krzan@icloud.com>
2016-08-29 17:31:42 +0200

    Merged Themes

--- packages.sh
+++ packages.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/iPower debs
 dpkg-deb -bZgzip projects/iPowerRepoIcons debs
-dpkg-deb -bZgzip projects/iPowerLockGlyph debs
-dpkg-deb -bZgzip projects/iPowerBioProtect debs
 
