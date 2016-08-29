19aa4ac7dbf0c88b0e5c97b01a6dd78dbb54cbe7
iPower <jan.krzan@icloud.com>
2016-08-29 18:15:43 +0200

    Fixed issues

--- packages.sh
+++ packages.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/iPower debs
 dpkg-deb -bZgzip projects/iPowerRepoIcons debs
-
+dpkg-deb -bZgzip projects/iPowerLockGlyph debs
+dpkg-deb -bZgzip projects/iPowerLI debs
